575c1d29127483ae3094ca813cf50e9896b10c49
Edmore Moyo <etmoyo@gmail.com>
2013-02-05 19:59:54 +0200

    refactor feeds script

--- bin/feeds.rb
+++ bin/feeds.rb
@@ -17,8 +17,14 @@ venue_list.each do |v_id|
   login_cridentials = "-u #{cam_user} #{cam_password}" unless cam_user == ""
 
   cmds << "openRTSP #{login_cridentials} -F #{venue_name} -d 10 -b 200000 #{cam_url} && ffmpeg -i #{venue_name}video-H264-1 -r 1 -s 320x240 -ss 5 -vframes 1 -f image2 ../public/feeds/#{venue_name}/#{venue_name}.jpeg && rm -f #{venue_name}video-H264-1"
-  redis.set("venue:#{v_id}:last_updated", Time.now)
 end
-puts cmds.inspect
 
+puts cmds.inspect
 system cmds.join("&&")
+
+# Update the time the image was modified
+venue_list.each do |v_id|
+  venue_name = redis.get("venue:#{v_id}:venue_name")
+  next unless File.exist?("../public/feeds/#{venue_name}/#{venue_name}.jpeg")
+  redis.set("venue:#{v_id}:last_updated", File.mtime("../public/feeds/#{venue_name}/#{venue_name}.jpeg"))
+end
