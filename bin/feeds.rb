7e26e30e775e95be9e37ba4bd54218133c6c94ac
Edmore Moyo <etmoyo@gmail.com>
2013-02-12 15:48:47 +0200

    make the app root more dynamic

--- bin/feeds.rb
+++ bin/feeds.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'redis'
 
+app_root = ARGV[0]
 redis = Redis.new
 cmds = []
 venue_list = redis.lrange("venues", 0, -1)
@@ -14,7 +15,7 @@ venue_list.each do |v_id|
   cam_password = redis.get("venue:#{v_id}:cam_password")
   login_cridentials = "-u #{cam_user} #{cam_password}" unless cam_user == ""
 
-  cmds << "openRTSP #{login_cridentials} -F #{venue_name} -d 10 -b 300000 #{cam_url} && ffmpeg -i #{venue_name}video-H264-1 -r 1 -s 320x240 -ss 5 -vframes 1 -f image2 ../public/feeds/#{venue_name}/#{venue_name}.jpeg && rm -f #{venue_name}video-H264-1"
+  cmds << "openRTSP #{login_cridentials} -F #{venue_name} -d 10 -b 300000 #{cam_url} && ffmpeg -i #{venue_name}video-H264-1 -r 1 -s 320x240 -ss 5 -vframes 1 -f image2 #{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg && rm -f #{venue_name}video-H264-1"
 end
 
 puts cmds.inspect
@@ -23,6 +24,6 @@ system cmds.join("&&")
 # Update the time the image was modified
 venue_list.each do |v_id|
   venue_name = redis.get("venue:#{v_id}:venue_name")
-  next unless File.exist?("../public/feeds/#{venue_name}/#{venue_name}.jpeg")
-  redis.set("venue:#{v_id}:last_updated", File.mtime("../public/feeds/#{venue_name}/#{venue_name}.jpeg"))
+  next unless File.exist?("#{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg")
+  redis.set("venue:#{v_id}:last_updated", File.mtime("#{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg"))
 end
