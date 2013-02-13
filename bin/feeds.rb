66a780924f50a9a5bfc6bb6ac3da7bf6b81c7c29
Edmore Moyo <etmoyo@gmail.com>
2013-02-13 10:53:53 +0200

    making the script play nice with cron and ruby

--- bin/feeds.rb
+++ bin/feeds.rb
@@ -1,9 +1,8 @@
-#! /usr/bin/env ruby
+#! /usr/local/bin/ruby
 
 require 'rubygems'
 require 'redis'
 
-app_root = ARGV[0]
 redis = Redis.new
 cmds = []
 venue_list = redis.lrange("venues", 0, -1)
@@ -15,15 +14,14 @@ venue_list.each do |v_id|
   cam_password = redis.get("venue:#{v_id}:cam_password")
   login_cridentials = "-u #{cam_user} #{cam_password}" unless cam_user == ""
 
-  cmds << "openRTSP #{login_cridentials} -F #{venue_name} -d 10 -b 300000 #{cam_url} && ffmpeg -i #{venue_name}video-H264-1 -r 1 -s 320x240 -ss 5 -vframes 1 -f image2 #{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg && rm -f #{venue_name}video-H264-1"
+  cmds << "openRTSP #{login_cridentials} -F #{venue_name} -d 10 -b 300000 #{cam_url} && ffmpeg -i #{venue_name}video-H264-1 -r 1 -s 320x240 -ss 5 -vframes 1 -f image2 /usr/local/camera_dashboard/public/feeds/#{venue_name}/#{venue_name}.jpeg && rm -f #{venue_name}video-H264-1"
 end
 
-puts cmds.inspect
-system cmds.join("&&")
+cmds.each{|c| system c; puts c}
 
 # Update the time the image was modified
 venue_list.each do |v_id|
   venue_name = redis.get("venue:#{v_id}:venue_name")
-  next unless File.exist?("#{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg")
-  redis.set("venue:#{v_id}:last_updated", File.mtime("#{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg"))
+  next unless File.exist?("/usr/local/camera_dashboard/public/feeds/#{venue_name}/#{venue_name}.jpeg")
+  redis.set("venue:#{v_id}:last_updated", File.mtime("/usr/local/camera_dashboard/public/feeds/#{venue_name}/#{venue_name}.jpeg"))
 end
