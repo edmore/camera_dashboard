4c9ef90d3e30a75f60b901fdc41cb0f84b8af410
Edmore Moyo <etmoyo@gmail.com>
2013-02-22 15:14:13 +0200

    update to setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,6 +3,6 @@
 crontab -l > tmp_file
 echo "\nPATH=/usr/local/bin\n
 # Setup camera feeds script to run every 5mins\n
-*/5 * * * * ruby /usr/local/camera_dashboard/bin/feeds.rb 2 > /dev/null" >> tmp_file
+*/5 * * * * ruby /usr/local/camera_dashboard/bin/feeds.rb 2> /dev/null" >> tmp_file
 crontab tmp_file
 rm -f tmp_file
