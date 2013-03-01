456382fa4baaf6e5ce9b61ffb6c3d6ffc7651cdc
Edmore Moyo <etmoyo@gmail.com>
2013-03-01 15:16:33 +0200

    refactoring

--- camera_dashboard.rb
+++ camera_dashboard.rb
@@ -1,49 +1,50 @@
 require 'sinatra'
 require 'haml'
-require 'redis'
+require 'REDIS'
 require 'time'
 require_relative 'matterhornconfig.rb'
 
 include MatterhornConfig
 
 set :haml, :format => :html5
-redis = Redis.new
+REDIS = Redis.new
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   [username, password] == MatterhornConfig::IPCam::AUTH
 end
 
-get "/" do
-  venues = []
-  venue_list = redis.lrange("venues", 0, -1)
+helpers do
+  def get_venues
+    today = Time.now
+    venues = []
+    venue_list = REDIS.lrange("venues", 0, -1)
 
-  venue_list.each_with_index do |v_id, i|
-    venues[i] = []
-    venues[i] << redis.get("venue:#{v_id}:venue_name")
-    venues[i] << redis.get("venue:#{v_id}:cam_url")
-    venues[i] << v_id
-    venues[i] << redis.get("venue:#{v_id}:last_updated")
+    venue_list.each_with_index do |v_id, i|
+      venues[i] = []
+      venues[i] << REDIS.get("venue:#{v_id}:venue_name")
+      venues[i] << REDIS.get("venue:#{v_id}:cam_url")
+      venues[i] << v_id
+      venues[i] << Time.parse(REDIS.get("venue:#{v_id}:last_updated"))
+    end
+    puts venues.inspect
+    [venues, today]
   end
-  puts venues.inspect
+end
 
+get "/" do
+  venues = get_venues[0]
   haml :dashboard, :locals => {:venues => venues}
 end
 
 get "/tiled" do
-  today = Time.now
-  venues = []
-  venue_list = redis.lrange("venues", 0, -1)
-
-  venue_list.each_with_index do |v_id, i|
-    venues[i] = []
-    venues[i] << redis.get("venue:#{v_id}:venue_name")
-    venues[i] << redis.get("venue:#{v_id}:cam_url")
-    venues[i] << v_id
-    venues[i] << Time.parse(redis.get("venue:#{v_id}:last_updated"))
-  end
-  puts venues.inspect
-
+  venues = get_venues[0]
+  today = get_venues[1]
   haml :tiled, :locals => {:venues => venues, :today => today}
+end
+
+get "/venues" do
+  venues = get_venues[0]
+  haml :venues, :locals => {:venues => venues}
 end
 
 post "/venue" do
@@ -52,12 +53,12 @@ post "/venue" do
   puts params.inspect
 
   unless (params[:venue_name] == "" || params[:cam_url] == "")
-    venue_id = redis.incr "venue:id"
-    redis.rpush("venues", venue_id)
-    redis.set("venue:#{venue_id}:venue_name", params[:venue_name].downcase)
-    redis.set("venue:#{venue_id}:cam_user", params[:cam_user])
-    redis.set("venue:#{venue_id}:cam_password", params[:cam_password])
-    redis.set("venue:#{venue_id}:cam_url", params[:cam_url])
+    venue_id = REDIS.incr "venue:id"
+    REDIS.rpush("venues", venue_id)
+    REDIS.set("venue:#{venue_id}:venue_name", params[:venue_name].downcase)
+    REDIS.set("venue:#{venue_id}:cam_user", params[:cam_user])
+    REDIS.set("venue:#{venue_id}:cam_password", params[:cam_password])
+    REDIS.set("venue:#{venue_id}:cam_url", params[:cam_url])
 
     cmds << "mkdir public/feeds/#{params[:venue_name]}/"
     system cmds.join("&&")
@@ -66,43 +67,29 @@ post "/venue" do
   redirect '/venues'
 end
 
-get "/venues" do
-  venues = []
-  venue_list = redis.lrange("venues", 0, -1)
-
-  venue_list.each_with_index do |v_id, i|
-    venues[i] = []
-    venues[i] << redis.get("venue:#{v_id}:venue_name")
-    venues[i] << redis.get("venue:#{v_id}:cam_url")
-    venues[i] << v_id
-  end
-
-  haml :venues, :locals => {:venues => venues}
-end
-
 get "/venue/:id" do
-    venue = []
-    v_id = params[:id]
+  venue = []
+  v_id = params[:id]
 
-    venue << redis.get("venue:#{v_id}:venue_name")
-    venue << redis.get("venue:#{v_id}:cam_user")
-    venue << redis.get("venue:#{v_id}:cam_password")
-    venue << redis.get("venue:#{v_id}:cam_url")
-    venue << v_id
+  venue << REDIS.get("venue:#{v_id}:venue_name")
+  venue << REDIS.get("venue:#{v_id}:cam_user")
+  venue << REDIS.get("venue:#{v_id}:cam_password")
+  venue << REDIS.get("venue:#{v_id}:cam_url")
+  venue << v_id
 
-    haml :venue_edit, :locals => {:venue => venue}
+  haml :venue_edit, :locals => {:venue => venue}
 end
 
 put "/venue/:id" do
   status = ""
   cmds = []
   v_id = params[:id]
-  old_venue_name = redis.get("venue:#{v_id}:venue_name")
+  old_venue_name = REDIS.get("venue:#{v_id}:venue_name")
 
-  redis.set("venue:#{v_id}:venue_name", params[:venue_name].downcase)
-  redis.set("venue:#{v_id}:cam_user", params[:cam_user])
-  redis.set("venue:#{v_id}:cam_password", params[:cam_password])
-  redis.set("venue:#{v_id}:cam_url", params[:cam_url])
+  REDIS.set("venue:#{v_id}:venue_name", params[:venue_name].downcase)
+  REDIS.set("venue:#{v_id}:cam_user", params[:cam_user])
+  REDIS.set("venue:#{v_id}:cam_password", params[:cam_password])
+  REDIS.set("venue:#{v_id}:cam_url", params[:cam_url])
 
   if old_venue_name != params[:venue_name]
     cmds << "mv public/feeds/#{old_venue_name}/ public/feeds/#{params[:venue_name]}/"
@@ -121,10 +108,10 @@ end
 
 delete "/venue/:id" do
   v_id = params[:id]
-  venue_name = redis.get("venue:#{v_id}:venue_name")
+  venue_name = REDIS.get("venue:#{v_id}:venue_name")
 
-  redis.lrem("venues", 0, v_id)
-  redis.del("venue:#{v_id}:venue_name", "venue:#{v_id}:cam_user", "venue:#{v_id}:cam_password", "venue:#{v_id}:cam_url")
+  REDIS.lrem("venues", 0, v_id)
+  REDIS.del("venue:#{v_id}:venue_name", "venue:#{v_id}:cam_user", "venue:#{v_id}:cam_password", "venue:#{v_id}:cam_url")
   system("rmdir public/feeds/#{venue_name}")
 
   redirect '/venues'
