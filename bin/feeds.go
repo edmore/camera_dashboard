2d61410d8c8c7600ca3cf68059ac71f424a0ac5d
Edmore Moyo <etmoyo@gmail.com>
2013-04-11 12:36:25 +0200

    some formatting using gofmt

--- bin/feeds.go
+++ bin/feeds.go
@@ -1,11 +1,11 @@
 package main
 
-import(
+import (
 	"github.com/garyburd/redigo/redis"
-        "log"
+	"log"
+	"os"
 	"os/exec"
-        "os"
-        "sync"
+	"sync"
 )
 
 func getPath(p string) string {
@@ -21,49 +21,49 @@ func checkError(err error) {
 }
 
 func main() {
-        var wg sync.WaitGroup
+	var wg sync.WaitGroup
 	app_root := "/usr/local/camera_dashboard"
-        c, err := redis.Dial("tcp", ":6379")
-        defer c.Close()
+	c, err := redis.Dial("tcp", ":6379")
+	defer c.Close()
 	checkError(err)
 	venue_list, _ := redis.Strings(c.Do("LRANGE", "venues", 0, -1))
 
 	for _, v := range venue_list {
-                venue := make(map[string]string)
-		options := []string{ "venue_name", "cam_url", "cam_user", "cam_password" }
+		venue := make(map[string]string)
+		options := []string{"venue_name", "cam_url", "cam_user", "cam_password"}
 		ffmpeg := getPath("ffmpeg")
 		openRTSP := getPath("openRTSP")
 		login_cridentials := ""
 
 		for _, o := range options {
-			venue[o], _ =  redis.String(c.Do("GET", "venue:" + v + ":" + o))
+			venue[o], _ = redis.String(c.Do("GET", "venue:"+v+":"+o))
 		}
 
-		if (venue["cam_user"] != ""){
-			login_cridentials = "-u "+venue["cam_user"]+" "+venue["cam_password"]
+		if venue["cam_user"] != "" {
+			login_cridentials = "-u " + venue["cam_user"] + " " + venue["cam_password"]
 		}
 
-		go func(v string){
+		go func(v string) {
 			wg.Add(1)
-			dir := app_root+"/public/feeds/"+venue["venue_name"]
+			dir := app_root + "/public/feeds/" + venue["venue_name"]
 			os.MkdirAll(dir, 0755)
-			feed_cmd := openRTSP+` `+login_cridentials+ ` -F `+venue["venue_name"]+` -d 10 -b 300000 `+venue["cam_url"]+` \
-                                            && `+ffmpeg+` -i `+venue["venue_name"]+`video-H264-1 -r 1 -s 320x180 -ss 5 -vframes 1\
-                                            -f image2 `+app_root+`/public/feeds/`+venue["venue_name"]+`/`+venue["venue_name"]+`.jpeg\
-                                            && rm -f `+venue["venue_name"]+`video-H264-1`
+			feed_cmd := openRTSP + ` ` + login_cridentials + ` -F ` + venue["venue_name"] + ` -d 10 -b 300000 ` + venue["cam_url"] + ` \
+                                            && ` + ffmpeg + ` -i ` + venue["venue_name"] + `video-H264-1 -r 1 -s 320x180 -ss 5 -vframes 1\
+                                            -f image2 ` + app_root + `/public/feeds/` + venue["venue_name"] + `/` + venue["venue_name"] + `.jpeg\
+                                            && rm -f ` + venue["venue_name"] + `video-H264-1`
 			cmd := exec.Command("bash", "-c", feed_cmd)
-                        // run command
+			// run command
 			err = cmd.Run()
 
-                        // update the last_updated date
-                        image := app_root+"/public/feeds/"+venue["venue_name"]+"/"+venue["venue_name"]+".jpeg"
+			// update the last_updated date
+			image := app_root + "/public/feeds/" + venue["venue_name"] + "/" + venue["venue_name"] + ".jpeg"
 			_, err := os.Open(image)
 
 			// returns true if it gets "no such file or directory" error
 			if !os.IsNotExist(err) {
 				stats, err := os.Stat(image)
 				checkError(err)
-				c.Do("SET","venue:"+v+":last_updated", stats.ModTime())
+				c.Do("SET", "venue:"+v+":last_updated", stats.ModTime())
 			}
 			wg.Done()
 		}(v)
