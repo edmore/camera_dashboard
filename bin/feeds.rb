ba7077df2989b90b5b207f7f8aa7dd8a2df84f25
Edmore Moyo <etmoyo@gmail.com>
2013-04-12 12:33:56 +0200

    make the script more ruby like

--- bin/feeds.rb
+++ bin/feeds.rb
@@ -10,14 +10,13 @@ cmds = []
 venue_list = redis.lrange("venues", 0, -1)
 
 venue_list.each do |v_id|
-  venue_name = redis.get("venue:#{v_id}:venue_name")
-  cam_url = redis.get("venue:#{v_id}:cam_url")
-  cam_user = redis.get("venue:#{v_id}:cam_user")
-  cam_password = redis.get("venue:#{v_id}:cam_password")
-  login_cridentials = "-u #{cam_user} #{cam_password}" unless cam_user == ""
+  venue = {}
+  options = ["venue_name", "cam_url", "cam_user", "cam_password"]
+  options.each{ |o| venue[o] = redis.get("venue:#{v_id}:#{o}") }
+  login_cridentials = "-u #{venue["cam_user"]} #{venue["cam_password"]}" unless venue["cam_user"] == ""
 
-  FileUtils.mkdir_p("#{app_root}/public/feeds/#{venue_name}/")
-  cmds << "openRTSP #{login_cridentials} -F #{venue_name} -d 10 -b 300000 #{cam_url} 2> /dev/null && ffmpeg -i #{venue_name}video-H264-1 -r 1 -s 1280x720 -ss 5 -vframes 1 -f image2 #{app_root}/public/feeds/#{venue_name}/#{venue_name}_big.jpeg 2>/dev/null && ffmpeg -i #{app_root}/public/feeds/#{venue_name}/#{venue_name}_big.jpeg -s 320x180 -f image2 #{app_root}/public/feeds/#{venue_name}/#{venue_name}.jpeg 2>/dev/null  && rm -f #{venue_name}video-H264-1 2>/dev/null"
+  FileUtils.mkdir_p("#{app_root}/public/feeds/#{venue["venue_name"]}/")
+  cmds << "openRTSP #{login_cridentials} -F #{venue["venue_name"]} -d 10 -b 300000 #{venue["cam_url"]} 2> /dev/null && ffmpeg -i #{venue["venue_name"]}video-H264-1 -r 1 -s 1280x720 -ss 5 -vframes 1 -f image2 #{app_root}/public/feeds/#{venue["venue_name"]}/#{venue["venue_name"]}_big.jpeg 2>/dev/null && ffmpeg -i #{app_root}/public/feeds/#{venue["venue_name"]}/#{venue["venue_name"]}_big.jpeg -s 320x180 -f image2 #{app_root}/public/feeds/#{venue["venue_name"]}/#{venue["venue_name"]}.jpeg 2>/dev/null  && rm -f #{venue["venue_name"]}video-H264-1 2>/dev/null"
 end
 
 cmds.each{|c| system c }
