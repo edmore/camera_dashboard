eb8c3a2186c8bcd9263f3df4b49866cec5820c91
Edmore Moyo <etmoyo@gmail.com>
2013-05-03 15:43:09 +0200

    Added some specs to the dashboard code

--- spec/camera_dashboard_spec.rb
+++ spec/camera_dashboard_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../peekaboo.rb'
+require_relative '../camera_dashboard.rb'
 require 'rack/test'
 require 'capybara/rspec'
 
@@ -16,10 +16,34 @@ RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 end
 
-feature  "Index page" do
-setup
+feature  "Dashboard page" do
+  setup
+  scenario "should display the dashboard" do
+    page.driver.browser.authorize 'admin', 'admin'
+    visit("/")
+    page.should have_content("IP Camera Dashboard")
+  end
 end
 
-feature  "Venue page" do
+feature  "Tiled page" do
   setup
+  scenario "should display the tiled page" do
+    page.driver.browser.authorize 'admin', 'admin'
+    visit("/tiled")
+    page.should have_content("IP Camera Dashboard")
+    page.should have_content("Venue")
+    page.should have_content("Last Updated")
+  end
+end
+
+feature "Manage Page" do
+  setup
+  scenario "should display the manage page" do
+    page.driver.browser.authorize 'admin', 'admin'
+    visit("/venues")
+    page.should have_content("IP Camera Information")
+    page.should have_link("Dashboard")
+    page.should have_content("Venue Name")
+    page.should have_content("Last Synced")
+  end
 end
